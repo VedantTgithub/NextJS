9d202b21fcb3396e30a757b7742eaa954dff79f6
Vedant <vedant0711@gmail.com>
2024-01-20 16:20:52 +0530

    Auth

---
+++ uber_app/app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,15 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <>
+      <div>
+        <Image src='/banner.jpg' width={900} height={1000} className="object-contain h-full w-full" />
+        <div className="absolute top-10 left-10">
+        <SignIn />
+        </div>
+      </div>
+    </>
+  );
+}
