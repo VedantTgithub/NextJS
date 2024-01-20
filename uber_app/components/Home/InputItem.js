91961f6d21a82cb61761847af8f9cb71c65edc92
Vedant <vedant0711@gmail.com>
2024-01-21 02:11:11 +0530

    API

--- uber_app/components/Home/InputItem.js
+++ uber_app/components/Home/InputItem.js
@@ -1,12 +1,22 @@
-import React from 'react'
+"use client"
+import React,{ useState } from 'react';
 import Image from 'next/image'
-
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 function InputItem({type}) {
+    const [value, setValue] = useState(null);
   return (
     <div className='bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4'>
         <Image src='/Source.jpeg' width={15} height={15}/>
-        <input type='text' placeholder={type=='source'?'Pickup Location':'DropOff Location'}
+        {/* <input type='text' placeholder={type=='source'?'Pickup Location':'DropOff Location'}
         className='bg-transparent w-full outline-none'/>
+         */}
+            <GooglePlacesAutocomplete
+            apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+            selectProps={{
+                value,
+                onChange: setValue,
+              }}
+               />
         
     </div>
   )
